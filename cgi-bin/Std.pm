a93008ac895c97a61ce84f5ce6efc289d8eb0a27
Leonardo <leo-94@hotmail.it>
2016-05-20 16:28:43 +0200

    versione leo

--- cgi-bin/Std.pm
+++ cgi-bin/Std.pm
@@ -1,4 +1,5 @@
 #!/usr/bin/perl
+use DateTime;
 use strict;
 use warnings;
 use CGI;
@@ -11,7 +12,7 @@ our @EXPORT = qw(HtmlCode);
 sub HtmlCode{
   print "Content-type: text/html\n\n";
 
-print" 
+print"
 <!DOCTYPE html PUBLIC \\'-//W3C//DTD XHTML 1.0 Strict//EN'' \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
 <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"it\" lang=\"it\">
 
@@ -32,12 +33,13 @@ print"
 <body>
 <div id=\"wrapper\">
 <div id=\"header\">
-<h1>Bed and Breakfast Navona - Roma</h1>
+    <img src=\"../img/Logo-bn.png\" alt=\"logo B&B\" id=\"logo\">
+<!--<h1>Bed and Breakfast Navona - Roma</h1>-->
 </div>
 
 <div id=\"menu\">
 	<ul>
-		<li id=\"currentLink\"><a href=\"index.html\"><span lang=\"en\">Home</span></a></li>
+		<li><a href=\"index.html\"><span lang=\"en\">Home</span></a></li>
 
     	<li><a href=\"chisiamo.html\">Chi siamo</a></li>
 
@@ -47,16 +49,20 @@ print"
 
     	<li><a href=\"tariffe.html\">Tariffe</a></li>
 
-    	<li><a href=\"prenotazioni.html\">Prenotazioni</a></li>
+    	<li id=\"currentLink\"><a href=\"prenotazioni.html\">Prenotazioni</a></li>
 
     	<li><a href=\"contatti.html\">Contatti</a></li>
 	</ul>
+</div>
+
+<div id=\"breadcrumb\">
+<p>Ti trovi qui :<a href=\"index.html\"><span lang=\"en\">Home</span></a> &gt;&gt; <a href=\"prenotazioni.html\"><span lang=\"en\">Prenotazioni</span></a> &gt;&gt; Disponibilit&agrave;</p>
 </div>
 ";
 }
 
 sub PrintPren{
-my($arrivo,$notti,$adulti,$camera,$numeroprenotazione)=@_;
+my($arrivo,$notti,$adulti,$camera,$prezzo,$numeroprenotazione)=@_;
 print "<h2>La tua prenotazione \n</h2>";
 print "Numero prenotazione".$numeroprenotazione."\n";
 print "<div id=\"prenotazione\">";
@@ -64,11 +70,164 @@ print "<div id=\"prenotazione\">";
 print "Notti ".$notti->textContent."\n";
 print "Adulti ".$adulti->textContent."\n";
 print "Tipo Camera ".$camera->textContent."\n";
+print "Totale €: ".$prezzo->textContent."\n";
 print "</div>";
 }
 
+sub Disp{
+my($dataarrivo,$datapartenza,$numerocamere,$adulti,$doppie,$singole,$exdoppie,$exsingole)=@_;
+print "<h2>La tua prenotazione \n</h2>";
+print "<div id=\"richiesta\">";
+print "<p>Data Arrivo ".$dataarrivo."\n</p>";
+print "<p>Data Partenza ".$datapartenza."\n</p>";
+print "<p>Adulti ".$adulti."\n</p>";
+print "<p>Numero Camere ".$numerocamere."\n</p>";
+# print "</div>";
+# my $doppie = int($numerocamere / 2);
+# my $singole = int($numerocamere % 2);
+my $exdoppie="";
+my $exsingole="";
+if($doppie != 0){
+    if($doppie == 1){
+    $exdoppie="$doppie camera doppia e ";
+    }
+    else{
+    $exdoppie="$doppie camere doppie e ";
+    }
+    }
+if($singole != 0){
+    if($    singole == 1){
+    $exsingole="$singole camera singola";
+    }
+    else{
+    $exsingole="$singole camere singole";
+    }
+    }
+
+print "<p>La tua prenotazione comprende: $exdoppie $exsingole.</p>";
+print "</div>"
+#print "Totale €: ".$prezzo->textContent."\n";
+#print "</div>";
+}
+
+sub Servizi{
+  print "Servizi";
+my ($parcheggio,$pulizia,$navettaaereo, $navettatreno)=@_;
+  print "<div id = \"dettagli\">";
+  print "<p>Hai richiesto anche: </p>";
+   if($parcheggio eq "true"){
+   print "<p>- Parcheggio</p>";
+   }
+   if($pulizia eq "true"){
+   print "<p>- Pulizia Quotidiana</p>";
+   }
+   if($navettaaereo eq "true"){
+   print "<p>- Navetta dall'Aeroporto</p>";
+   }
+   if($navettatreno eq "true"){
+   print "<p>Navetta dalla Stazione</p>";
+   }
+   #print "</div>";
+}
+sub Prezzi{
+  print "<div id = \"dettprezzi\">";
+    my($dataarrivo,$datapartenza,$doppie,$singole,$parcheggio,$pulizia,$navettaaereo,$navettatreno,$diff)=@_;
+          my $prdoppie = "0";
+          my $prsingole = "0";
+          my $prparcheggio = "0";
+          my $prpulizia = "0";
+          my $prnavettaaereo = "0";
+          my $prnavettatreno = "0";
+      if($doppie > 0){
+          $prdoppie = (30*$doppie);
+          if($doppie == 1){
+          print "<p>$doppie camera doppia &nbsp; 30 x $doppie = &euro; $prdoppie.</p>";
+          }
+          else{
+            print "<p>$doppie camere doppie &nbsp; 30 x $doppie = &euro; $prdoppie.</p>";
+          }
+          }
+      if($singole > 0){
+          $prsingole = (20*$singole);
+          if($singole == 1){
+          print "<p>$singole camera singola &nbsp; 20 x $singole = &euro; $prsingole.</p>";
+          }
+          else{
+          print "<p>$singole camere singole &nbsp; 20 x $singole = &euro; $prsingole.</p>";
+          }
+          }
+      if($parcheggio eq "true"){
+        my $prpark = "5";
+          $prparcheggio = $prpark*$diff;
+        print "<p>Parcheggio coperto = &euro; $prpark x $diff giorni = &euro; $prparcheggio</p>";
+      }
+      if($pulizia eq "true"){
+        my $prpul = "2";
+          $prpulizia = $prpul*$diff;
+        print "<p>Pulizia quotidiana = &euro; $prpul x $diff giorni = &euro; $prpulizia</p>";
+      }
+      if($navettaaereo eq "true"){
+          $prnavettaaereo = "30";
+        print "<p>Navetta B&B Navona - Aeroporto Fiumicino = &euro; $prnavettaaereo</p>";
+      }
+
+      if($navettatreno eq "true"){
+          $prnavettatreno = "30";
+        print "<p>Navetta B&B Navona - Stazione Termini = &euro; $prnavettatreno</p>";
+      }
+      my $totale = $prdoppie + $prsingole + $prparcheggio + $prpulizia + $prnavettaaereo + $prnavettatreno;
+      print "<p>Totale &euro; $totale</p>";
+      print "</div>";
+    }
+
+sub DiffData{
+  my ($dataarrivo,$datapartenza)=@_;
+  $dataarrivo =~ m!^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/]((19|20)\d\d)$!;
+  my $dt1 = DateTime->new(
+      year       => $3,
+      month      => $2,
+      day        => $1,
+      hour       => 00,
+      minute     => 00,
+      second     => 00,
+      nanosecond => 000000000,
+      time_zone  => 'Europe/Rome',
+  );
+  $datapartenza =~ m!^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/]((19|20)\d\d)$!;
+  my $dt2 = DateTime->new(
+      year       => $3,
+      month      => $2,
+      day        => $1,
+      hour       => 00,
+      minute     => 00,
+      second     => 00,
+      nanosecond => 000000000,
+      time_zone  => 'Europe/Rome',
+  );
+  my $duration = $dt1->delta_days($dt2);
+  return $duration->days;
+}
+
 sub EndHtml{
-print "</div>
+print "<div id=\"footer\">
+    <div id=\"left\">
+    <p>Tutti i diritti riservati</p>
+	<img class=\"imgValidCode\" src=\"../img/XMLvalido.png\" alt=\"XHTML valido\"/>
+	<img class=\"imgValidCode\" src=\"../img/CSSvalido.gif\" alt=\"CSS valido\"/>
+    </div>
+
+    <div id=\"right\">
+        <h3> Contatti </h3>
+		<p>Via delle Pertiche, 11
+    	    04323 Latina
+            Roma (RM)</p>
+
+        <p>Telefono 0093412534</p>
+        <p>Email bbmichele\@gmail.com</p>
+        </div>
+
+</div>
+</div>
 </body>
 </html>";
 }
---
+++ cgi-bin/disponibilita.pl
@@ -0,0 +1,194 @@
+#!/usr/bin/perl
+#print "Content type: text/html; charset=UTF-8\n\n";
+
+#possibile modifica per gestire auto_increment,
+#creare attributo su tag prenotazioni con contatore prenotazioni;
+
+use DateTime;
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+# use Encode qw(encode);
+use strict;
+use warnings;
+use Std;
+binmode STDOUT, ":utf8";
+
+Std::HtmlCode();
+
+sub inserisciDatiXML{
+#use XML::LibXML;
+#my $file = '../data/tariffe.xml';
+
+#my $parser = XML::LibXML->new();
+#my $id = -1;
+
+#my $doc=$parser->parse_file($file);
+
+my %prenotazione = @_;
+my $dataarrivo = $prenotazione{"dataArrivo"};
+my $datapartenza = $prenotazione{"dataPartenza"};
+#my $notti = "\n\t\t<notti>".$prenotazione{'notti'}."</notti>";
+my $numerocamere = $prenotazione{"numeroCamere"};
+my $adulti = $prenotazione{"adulti"};
+#my $tipocamera = "\n\t\t<tipoCamera>".$prenotazione{'tipoCamera'}."</tipoCamera>";
+my $parcheggio = $prenotazione{"parcheggio"};
+my $pulizia = $prenotazione{"pulizia"};
+my $navettaaereo = $prenotazione{"navettaereo"};
+my $navettatreno = $prenotazione{"navettatreno"};
+#print "Il numero della prenotazione &egrave; $id\n";
+
+}
+
+
+sub isvaliddate {
+  my $input = shift;
+  if ($input =~ m!^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/]((19|20)\d\d)$!) {
+    # At this point, $1 holds the day, $2 the month and $3 the year of the date entered
+    if ($1 == 31 and ($2 == 4 or $2 == 6 or $2 == 9 or $2 == 11)) {
+      return 0; # 31st of a month with 30 days
+    } elsif ($1 >= 30 and $2 == 2) {
+      return 0; # February 30th or 31st
+    } elsif ($2 == 2 and $1 == 29 and not ($3 % 4 == 0 and ($3 % 100 != 0 or $3 % 400 == 0))) {
+      return 0; # February 29th outside a leap year
+    } else {
+      return 1; # Valid date
+    }
+  } else {
+    return 0; # Not a date
+  }
+}
+
+
+
+my $page = new CGI;
+my $dataarrivo = $page->param("dataArrivo");
+my $datapartenza = $page->param("dataPartenza");
+my $numerocamere = $page->param("numeroCamere");
+my $adulti = $page->param("adulti");
+my $parcheggio = $page->param("parcheggio");
+my $pulizia = $page->param("pulizia");
+my $navettaaereo = $page->param("navettaaereo");
+my $navettatreno = $page->param("navettatreno");
+my $doppie = int($adulti / 2);
+my $singole = int($adulti % 2);
+#my $exdoppie="";
+#my $exsingole="";
+
+my $error=0;
+
+
+
+
+if(!isvaliddate($dataarrivo)){
+	$error=1;
+	print "La data di arrivo contiene un errore\n";
+}else{
+  $dataarrivo =~ m!^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/]((19|20)\d\d)$!;
+  my $dt1 = DateTime->new(
+      year       => $3,
+      month      => $2,
+      day        => $1,
+      hour       => 00,
+      minute     => 00,
+      second     => 00,
+      nanosecond => 000000000,
+      time_zone  => 'Europe/Rome',
+  );
+  my $dt2 = DateTime->now(time_zone=>'local');
+  if(DateTime->compare($dt1, $dt2) == -1){
+  $error=1;
+  print "Data di arrivo non valida\n ";
+  }
+}
+
+if(!isvaliddate($datapartenza)){
+	$error=1;
+	print "La data di partenza contiene un errore\n";
+}else{
+  $dataarrivo =~ m!^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/]((19|20)\d\d)$!;
+  my $dt1 = DateTime->new(
+      year       => $3,
+      month      => $2,
+      day        => $1,
+      hour       => 00,
+      minute     => 00,
+      second     => 00,
+      nanosecond => 000000000,
+      time_zone  => 'Europe/Rome',
+  );
+  my $dt2 = DateTime->now(time_zone=>'local');
+  if(DateTime->compare($dt1, $dt2) == -1){
+  $error=1;
+  print "Data di partenza non valida\n ";
+  }
+}
+
+
+
+#if ($notti !~ /\d/){
+#	$error=1;
+#	print "Notti non pu� contentere caratteri\n";
+#}elsif ($notti>=10){
+#	$error=1;
+#    print "Non puoi pernottare per pi� di 10 notti\n";
+#}elsif($notti == 0){
+#    $error=1;
+#	print "Il numero di notti non pu� essere pari a 0\n";
+#}
+
+if($datapartenza < $dataarrivo){
+    $error=1;
+    print "La data di partenza non pu&ograve; essere precedente a quella di arrivo\n";
+    # my $testo = enc($text);
+    # print $text;
+
+}
+
+#if ($adulti !~ /\d/){
+#	$error=1;
+#	print "Adulti non pu&ograve; contentere caratteri\n";
+#}elsif ($adulti>3){
+#	$error=1;
+#    print "Non esistono camere per pi&ugrave; di 3 adulti\n";
+#}elsif($adulti == 0){
+#	$error=1;
+#	print "Il numero di adulti non pu&ograve; essere pari a 0\n";
+#}
+
+# if ($tipocamera !~ /singola|doppia|singoladoppia/){
+# 	$error=1;
+# 	print "Non esiste questo tipo di camera\n";
+# }elsif ( ($tipocamera eq "singola" and $adulti > 1 ) or ( $tipocamera eq "doppia" and $adulti > 2 ) or ( $tipocamera eq "singoladoppia" # and $adulti > 3 ) ){
+# 	print "Troppi adulti per il tipo di camera\n";
+# 	$error=1;
+# }elsif ( ( $tipocamera eq "doppia" and $adulti < 2 ) or ( $tipocamera eq "singoladoppia" and $adulti < 3 )){
+# 	print "Pochi adulti per il tipo di camera\n";
+# 	$error=1;
+# }
+
+
+# if ($error){
+# 	print "Dati non corretti";
+# }else{
+	# inserisciDatiXML($page->Vars);
+# }
+
+if(!$error){
+    #print "no error";
+    Std::Disp($dataarrivo,$datapartenza,$numerocamere,$adulti,$doppie,$singole,);
+    if($parcheggio eq "true" || $pulizia eq "true" || $navettaaereo eq "true" || $navettatreno eq "true"){
+      Std::Servizi($parcheggio,$pulizia,$navettaaereo, $navettatreno);
+    }
+    my $diff = Std::DiffData($dataarrivo,$datapartenza);
+    Std::Prezzi($dataarrivo,$datapartenza,$doppie,$singole,$parcheggio,$pulizia,$navettaaereo,$navettatreno, $diff);
+    inserisciDatiXML($page->Vars);
+}
+else{
+    print "error";
+}
+
+# sub enc {
+#     return Encode::encode('UTF-8', $_[0]);
+# }
+
+Std::EndHtml();
