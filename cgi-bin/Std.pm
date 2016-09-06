19d430d4364ea103505eef2d9138f3a85e15caeb
black2279 <black.andrighetto94@gmail.com>
2016-09-06 02:51:37 +0200

    Fix link area riservata

--- cgi-bin/Std.pm
+++ cgi-bin/Std.pm
@@ -488,7 +488,7 @@ print "</div>
     </div>
 
 	<div id=\"reserved\">
-    <a href=\"cgi-bin/login.pl\">Area riservata</a>
+    <a href=\"login.pl\">Area riservata</a>
     </div>
 	
     <div id=\"right\">
